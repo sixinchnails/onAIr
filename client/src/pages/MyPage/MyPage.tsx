583358b0ca5eeb7500ad8f5f61915285b811f752
GyuhoChoi <s4248942@gmail.com>
2023-09-11 14:13:37 +0900

    FEAT : eslintrc.js 생성

--- client/src/pages/MyPage/MyPage.tsx
+++ client/src/pages/MyPage/MyPage.tsx
@@ -7,7 +7,6 @@ export const MyPage = () => {
     <div
       style={{ backgroundColor: "#000104", height: "100vh", color: "white" }}
     >
-      <div>안녕하세요</div>
       <NavBar />
       <h2>마이페이지</h2>
     </div>
