54867f9be00eafb95691c45fec36cd57ebb0151a
박기현 <qkrrlgus114@naver.com>
2023-09-11 11:49:38 +0900

    feat : refresh토큰 테스트중

--- zizon/src/test/java/com/b302/zizon/ZizonApplicationTests.java
+++ zizon/src/test/java/com/b302/zizon/ZizonApplicationTests.java
@@ -1,13 +1,24 @@
 package com.b302.zizon;
 
+import com.b302.zizon.domain.refreshtoken.dto.RefreshTokenCheckDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class ZizonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+
+//	@Test
+//	void refreshTokenCheckTest() {
+//		//given
+//		RefreshTokenCheckDTO refreshTokenCheckDTO = new RefreshTokenCheckDTO();
+//		refreshTokenCheckDTO.setRefreshToken("asgsdafsdagasdfsdafd");
+//		refreshTokenCheckDTO.setUserId(1L);
+//
+//	}
 
 }
