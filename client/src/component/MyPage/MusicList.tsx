65abd3d04e31003ea7eb942bcad3e2b5108bd29a
GyuhoChoi <s4248942@gmail.com>
2023-09-13 02:13:55 +0900

    feat : 라디오 + 버튼

--- client/src/component/MyPage/MusicList.tsx
+++ client/src/component/MyPage/MusicList.tsx
@@ -1,4 +1,14 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
 function MusicList() {
-  return <div>뮤직리스트</div>;
+  /**State*/
+  const userData = useSelector((state: RootState) => state.user); //사용자 정보를 Redux store에서 가져옵니다.
+
+  return (
+    <div>
+      <div>뮤직리스트</div>
+    </div>
+  );
 }
 export default MusicList;
