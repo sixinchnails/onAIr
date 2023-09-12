5bdd481735208110edd65ec17b00936fc0a272a9
강현 <hyunleo@naver.com>
2023-09-12 17:32:56 +0900

    FEAT: 09121732

--- client/src/component/PlayerPage/Radio.tsx
+++ client/src/component/PlayerPage/Radio.tsx
@@ -7,12 +7,16 @@ export const Radio = () => {
   const radioDummyData = useSelector((state: RootState) => state.radioDummy);
   const navigate = useNavigate();
 
+  //오디오가 종료되면 호출되는 함수
+  //INCREMENT_TTS_INDEX 액션을 디스패치하여 현재의 TTS의 인덱스를 증가시킴
+  //그 뒤 음악 플레이어 페이지로 옮겨줌
   const handleAudioEnd = () => {
     dispatch({ type: "INCREMENT_TTS_INDEX" }); // 인덱스 증가
     navigate("/MusicPlayer");
   };
 
-  // 현재 인덱스에 맞게 tts와 script 선택
+  // 배열 인덱싱을 사용하여 현재 TTS와 스크립트의 키를 선택
+  //radioDummyData.currentTTSIndex는 현재 재생해야 할 TTS의 인덱스
   const currentTTS = [`tts_one`, `tts_two`, `tts_three`, `tts_four`][
     radioDummyData.currentTTSIndex
   ];
