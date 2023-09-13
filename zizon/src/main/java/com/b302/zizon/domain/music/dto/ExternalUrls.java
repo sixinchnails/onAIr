6640da20a6e56fa5b35736e96a4779b94aa63228
박기현 <qkrrlgus114@naver.com>
2023-09-13 16:08:29 +0900

    Feat : 스포티파이 검색, 유튜브 검색 구현 완료

---
+++ zizon/src/main/java/com/b302/zizon/domain/music/dto/ExternalUrls.java
@@ -0,0 +1,14 @@
+package com.b302.zizon.domain.music.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ExternalUrls {
+  private String spotify;
+
+}
