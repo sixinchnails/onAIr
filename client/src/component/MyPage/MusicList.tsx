caea9b753662dea95344b641afaa912c938e68c0
GyuhoChoi <s4248942@gmail.com>
2023-09-14 00:27:52 +0900

    feat : 음악보관함 리스트

--- client/src/component/MyPage/MusicList.tsx
+++ client/src/component/MyPage/MusicList.tsx
@@ -2,17 +2,31 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { useState } from "react";
 import MusicBoxModal from "../Common/MusicBoxModal";
+import dummyData from "./dummyData.json";
+import AudiotrackIcon from "@mui/icons-material/Audiotrack";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+  Button,
+} from "@mui/material";
+import styles from "./MusicList.module.css";
 
 function MusicList() {
-  /**State*/
-  const userData = useSelector((state: RootState) => state.user); //사용자 정보를 Redux store에서 가져옵니다.
-  const [isMusicBoxModalOpen, setMusicBoxModalOpen] = useState<boolean>(false); //음악 보관함 모달
+  /** state */
+  const userData = useSelector((state: RootState) => state.user); //리덕스에 있는 회원데이터
+  const [isMusicBoxModalOpen, setMusicBoxModalOpen] = useState<boolean>(false);
+  const [myMusicBox, setMyMusicBox] = useState(dummyData.my_music_box);
+  const [playlistData, setPlaylistData] = useState(dummyData.playlist_info);
 
-  /**Action*/
   const MusicBoxModalOpen = () => {
     setMusicBoxModalOpen(true);
   };
-  //플레이리스트 모달 닫기 함수
+
   const MusicBoxModalClose = () => {
     setMusicBoxModalOpen(false);
   };
@@ -20,12 +34,73 @@ function MusicList() {
   return (
     <div>
       <div>뮤직리스트</div>
-      <button onClick={MusicBoxModalOpen}>+</button>
+      <button onClick={MusicBoxModalOpen}>보관함추가</button>
       <MusicBoxModal
         isOpen={isMusicBoxModalOpen}
         onClose={MusicBoxModalClose}
       ></MusicBoxModal>
+
+      <List>
+        <ListItem alignItems="flex-start" className={styles.hoverableListItem}>
+          <ListItemAvatar>
+            <AudiotrackIcon color="primary" style={{ fontSize: 40 }} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={
+              <Typography style={{ color: "#FF69B4" }}>전체보관함</Typography>
+            }
+            secondary={
+              <Typography
+                style={{ color: "#FF69B4" }}
+                component="span"
+                variant="body2"
+              >
+                {myMusicBox} 곡
+              </Typography>
+            }
+          />
+          <Button className={styles.playButton}>
+            <PlayCircleOutlineIcon />
+          </Button>
+        </ListItem>
+
+        {playlistData.map((playlist, index) => (
+          <ListItem
+            key={index}
+            alignItems="flex-start"
+            className={styles.hoverableListItem}
+          >
+            <ListItemAvatar>
+              <Avatar
+                variant="square"
+                src={playlist.playlistImage}
+                alt={playlist.playlistName}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <Typography style={{ color: "#FF69B4" }}>
+                  {playlist.playlistName}
+                </Typography>
+              }
+              secondary={
+                <Typography
+                  style={{ color: "#FF69B4" }}
+                  component="span"
+                  variant="body2"
+                >
+                  {playlist.playlistCount} 곡
+                </Typography>
+              }
+            />
+            <Button className={styles.playButton}>
+              <PlayCircleOutlineIcon />
+            </Button>
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
+
 export default MusicList;
