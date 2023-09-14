32b10a5e66f078e560c47eadb2a9b7a969f215c7
GyuhoChoi <s4248942@gmail.com>
2023-09-14 15:26:51 +0900

    feat : 리스트별 음악리스트 모달

--- client/src/component/MyPage/MusicDetailModal.tsx
+++ client/src/component/MyPage/MusicDetailModal.tsx
@@ -8,14 +8,20 @@ type MusicDetailModalProps = {
   isOpen: boolean;
   onClose: () => void;
   title: string;
+  songCount: number;
 };
 
-function MusicDetailModal({ isOpen, onClose, title }: MusicDetailModalProps) {
+function MusicDetailModal({
+  isOpen,
+  onClose,
+  title,
+  songCount,
+}: MusicDetailModalProps) {
   return (
     <Modal open={isOpen} onClose={onClose}>
       <Box className={styles.modalBox}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
-          {title} 노래 목록
+          {title} 노래 목록 - {songCount} 곡
         </Typography>
         <Button onClick={onClose}>닫기</Button>
       </Box>
