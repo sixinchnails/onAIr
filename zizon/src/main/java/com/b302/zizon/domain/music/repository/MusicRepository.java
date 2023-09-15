dc42c41f4bce6ef0a7c0982a8397cc854c46356b
박기현 <qkrrlgus114@naver.com>
2023-09-15 15:47:42 +0900

    Feat : 음악 검색 이후 유저 보관함에 추가

--- zizon/src/main/java/com/b302/zizon/domain/music/repository/MusicRepository.java
+++ zizon/src/main/java/com/b302/zizon/domain/music/repository/MusicRepository.java
@@ -4,6 +4,10 @@ import com.b302.zizon.domain.music.entity.Music;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MusicRepository extends JpaRepository<Music, Long> {
+
+    Optional<Music> findByYoutubeVideoId(String youtubeVideoId);
 }
