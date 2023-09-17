763e0da1de09b0a24ddf835942ab87132e654f42
강현 <hyunleo@naver.com>
2023-09-18 01:22:07 +0900

    마페-라디오 틀

--- client/src/component/MyPage/RadioCard.tsx
+++ client/src/component/MyPage/RadioCard.tsx
@@ -6,31 +6,52 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import CardMedia from "@mui/material/CardMedia";
+import AddIcon from "@mui/icons-material/Add";
+import AlertDialog from "../Common/AddPlayList";
+import { Button } from "@mui/material";
 
 type RecipeReviewCardProps = {
   title: string;
   subheader: string;
-  description: string;
   feeling: string;
   image: string;
 };
 
+type SongDataType = {
+  songTitle: string;
+  artist: string;
+  duration: string;
+  albumCover: string;
+}[];
+
 export default function RecipeReviewCard({
   title,
   subheader,
-  description,
-}: RecipeReviewCardProps) {
+  songs = [],
+  feeling,
+  image,
+}: RecipeReviewCardProps & { songs?: SongDataType }) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Card
       sx={{
         maxWidth: 345,
         borderRadius: "12px",
         boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
-        overflow: "hidden",
+        overflow: "auto",
         height: "300px",
       }}
     >
-      <CardContent>
+      <CardContent style={{ maxHeight: "300px" }}>
         <div
           style={{
             display: "flex",
@@ -49,12 +70,61 @@ export default function RecipeReviewCard({
             JOYFUL
           </Typography>
         </div>
-        <Typography variant="body2" color="text.secondary">
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          style={{ textAlign: "center" }}
+        >
           <h1>{subheader}</h1>
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
+        <hr />
+        {songs.map((song, idx) => (
+          <div
+            key={idx}
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginTop: "10px",
+              borderBottom: "1px solid #e5e5e5",
+              paddingBottom: "5px",
+            }}
+          >
+            <img
+              src={song.albumCover}
+              alt={`${song.songTitle} cover`}
+              style={{ width: "40px", height: "40px", marginRight: "10px" }}
+            />
+            <div style={{ flex: 2 }}>
+              <div>{song.songTitle}</div>
+              <div style={{ color: "#888", fontSize: "0.9em" }}>
+                {song.artist}
+              </div>
+            </div>
+            <div style={{ flex: 1, textAlign: "right" }}>{song.duration}</div>
+            <AddIcon
+              style={{ marginLeft: "8px" }}
+              onClick={handleClickOpen}
+              cursor="pointer"
+            />
+          </div>
+        ))}
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            gap: "10px",
+            justifyContent: "center",
+          }}
+        >
+          <Button variant="outlined" color="secondary">
+            삭제하기
+          </Button>
+          <Button variant="outlined" color="primary">
+            공유하기
+          </Button>
+        </div>
+
         <IconButton
           sx={{
             position: "absolute",
@@ -65,6 +135,7 @@ export default function RecipeReviewCard({
           aria-label="add to favorites"
         ></IconButton>
       </CardContent>
+      <AlertDialog open={open} handleClose={handleClose} />
     </Card>
   );
 }
