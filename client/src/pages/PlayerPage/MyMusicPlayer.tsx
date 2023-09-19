424f3acae59f893b25204724a87a0d012a5c47bf
강현 <hyunleo@naver.com>
2023-09-20 00:53:51 +0900

    axios없이 노래 더미데이터 페이지에 만들어서 음악 플레이어 페이지 틀

--- client/src/pages/PlayerPage/MyMusicPlayer.tsx
+++ client/src/pages/PlayerPage/MyMusicPlayer.tsx
@@ -4,6 +4,9 @@ import NavBar from "../../component/Common/Navbar";
 import { musicDummyData } from "./MusicDummy";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
+import styles from "./MyMusicPlayer.module.css";
+import SearchModal from "../../component/Common/SearchMusicModal";
+import DeleteModal from "../../component/MyPage/DeleteModal";
 
 type MyMusicPlayerProps = {};
 
@@ -26,119 +29,90 @@ export const MyMusicPlayer = () => {
     }
   };
 
+  const [isSearchModalOpen, setIsSearchModalOpen] = React.useState(false); // 모달의 열림/닫힘 상태를 관리하는 상태 변수
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = React.useState(false);
+  const handleSearchModalOpen = () => {
+    setIsSearchModalOpen(true);
+  };
+
+  const handleSearchModalClose = () => {
+    setIsSearchModalOpen(false);
+  };
+
+  const handleDeleteModalOpen = () => {
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeleteModalClose = () => {
+    setIsDeleteModalOpen(false);
+  };
+
   return (
-    <div
-      style={{ backgroundColor: "#000104", minHeight: "100vh", color: "white" }}
-    >
+    <div className={styles.root}>
       <NavBar />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: "20px",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "calc(100vh - 60px)",
-        }}
-      >
-        {/* 음악 컨테이너 상단 헤더 */}
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginBottom: "20px",
-          }}
-        >
-          <div>현재 재생 목록</div>
-          <AddCircleOutline />
-        </div>
-        {/* 이미지와 노래 리스트를 감싸는 컨테이너 */}
-        <div
-          style={{
-            flex: 1,
-            display: "flex",
-            width: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: "5%",
-            marginRight: "5%",
-          }}
-        >
-          {/* 이미지를 담는 컨테이너 */}
-          <div
-            style={{
-              flex: 1,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+      <div className={styles.container}>
+        <div className={styles.songDisplayContainer}>
+          <div className={styles.coverImageContainer}>
             <img
               src={musicDummyData[currentTrackIndex].cover}
               alt={musicDummyData[currentTrackIndex].title}
-              style={{ width: "500px", height: "500px" }}
+              className={styles.coverImage}
             />
           </div>
-          {/* 노래 리스트를 담는 컨테이너 */}
-          <div
-            style={{
-              flex: 0.5,
-              overflowY: "auto",
-              paddingRight: "20px",
-              maxHeight: "80vh",
-              marginLeft: "20%",
-            }}
-          >
-            {musicDummyData.map((song, index) => (
-              <div
-                key={index}
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  padding: "10px",
-                  backgroundColor:
-                    currentTrackIndex === index
-                      ? "rgba(255, 255, 255, 0.1)"
-                      : "transparent",
-                  marginBottom: "10px",
-                  cursor: "pointer",
-                  borderRadius: "5px",
-                }}
-                onClick={() => setCurrentTrackIndex(index)}
-              >
-                {/* 앨범 이미지 추가 */}
-                <img
-                  src={song.cover}
-                  alt={song.title}
-                  style={{ width: "50px", height: "50px", marginRight: "10px" }}
-                />
-                <div style={{ flex: 1 }}>
-                  <strong>{song.title}</strong>
-                  <br />
-                  {song.artist}
+          <div className={styles.songListContainer}>
+            <div className={styles.songListHeader}>
+              <div>현재 재생 목록</div>
+              <AddCircleOutline
+                onClick={handleSearchModalOpen}
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+            <div className={styles.songList}>
+              {musicDummyData.map((song, index) => (
+                <div
+                  key={index}
+                  className={`${styles.playlistItem} ${
+                    currentTrackIndex === index ? styles.playlistItemActive : ""
+                  }`}
+                  onClick={() => setCurrentTrackIndex(index)}
+                >
+                  <img
+                    src={song.cover}
+                    alt={song.title}
+                    className={styles.albumImage}
+                  />
+                  <div style={{ flex: 1 }}>
+                    <strong>{song.title}</strong>
+                    <br />
+                    {song.artist}
+                  </div>
+                  <div className={styles.songLength}>
+                    <DeleteOutlineIcon
+                      className={styles.deleteIcon}
+                      onClick={handleDeleteModalOpen}
+                    />
+                    {Math.floor(song.length / 60000)}:
+                    {String((song.length % 60000) / 1000).padStart(2, "0")}
+                  </div>
                 </div>
-                {/* 노래 길이 오른쪽 배치 */}
-                <div style={{ display: "flex", alignItems: "center" }}>
-                  <DeleteOutlineIcon style={{ marginRight: "10px" }} />{" "}
-                  {/* 쓰레기통 아이콘 추가 */}
-                  {Math.floor(song.length / 60000)}:
-                  {String((song.length % 60000) / 1000).padStart(2, "0")}{" "}
-                  {/* 음악 길이 형식 수정 */}
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-        {/* 오디오 플레이어를 담는 컨테이너 */}
-        <div style={{ width: "100%", textAlign: "center", marginTop: "20px" }}>
+        <div className={styles.audioContainer}>
           <audio ref={audioRef} onEnded={handleSongEnd} controls>
             <source type="audio/mp3" />
           </audio>
         </div>
       </div>
+      <SearchModal
+        isOpen={isSearchModalOpen}
+        onClose={handleSearchModalClose} // 모달 바깥쪽을 클릭하면 모달을 닫는다.
+      />
+      <DeleteModal
+        isOpen={isDeleteModalOpen}
+        onClose={handleDeleteModalClose} // 모달 바깥쪽을 클릭하면 모달을 닫는다.
+      />
     </div>
   );
 };
