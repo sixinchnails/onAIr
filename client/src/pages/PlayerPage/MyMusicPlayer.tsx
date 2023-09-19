505808c9d589e7083c7a968a239f81db81f6d4c5
강현 <hyunleo@naver.com>
2023-09-19 17:07:00 +0900

    d

--- client/src/pages/PlayerPage/MyMusicPlayer.tsx
+++ client/src/pages/PlayerPage/MyMusicPlayer.tsx
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../../component/Common/Navbar";
 import { musicDummyData } from "./MusicDummy";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
 
 type MyMusicPlayerProps = {};
 
@@ -39,6 +41,19 @@ export const MyMusicPlayer = () => {
           height: "calc(100vh - 60px)",
         }}
       >
+        {/* 음악 컨테이너 상단 헤더 */}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <div>현재 재생 목록</div>
+          <AddCircleOutline />
+        </div>
         {/* 이미지와 노래 리스트를 감싸는 컨테이너 */}
         <div
           style={{
@@ -47,6 +62,8 @@ export const MyMusicPlayer = () => {
             width: "100%",
             justifyContent: "center",
             alignItems: "center",
+            marginBottom: "5%",
+            marginRight: "5%",
           }}
         >
           {/* 이미지를 담는 컨테이너 */}
@@ -67,18 +84,21 @@ export const MyMusicPlayer = () => {
           {/* 노래 리스트를 담는 컨테이너 */}
           <div
             style={{
-              flex: 1,
+              flex: 0.5,
               overflowY: "auto",
               paddingRight: "20px",
               maxHeight: "80vh",
+              marginLeft: "20%",
             }}
           >
             {musicDummyData.map((song, index) => (
               <div
                 key={index}
                 style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
                   padding: "10px",
-                  width: "60%",
                   backgroundColor:
                     currentTrackIndex === index
                       ? "rgba(255, 255, 255, 0.1)"
@@ -89,12 +109,25 @@ export const MyMusicPlayer = () => {
                 }}
                 onClick={() => setCurrentTrackIndex(index)}
               >
-                <strong>{song.title}</strong>
-                <br />
-                {song.artist}
-                <br />
-                {Math.floor(song.length / 60000)}:
-                {((song.length % 60000) / 1000).toFixed(0)}
+                {/* 앨범 이미지 추가 */}
+                <img
+                  src={song.cover}
+                  alt={song.title}
+                  style={{ width: "50px", height: "50px", marginRight: "10px" }}
+                />
+                <div style={{ flex: 1 }}>
+                  <strong>{song.title}</strong>
+                  <br />
+                  {song.artist}
+                </div>
+                {/* 노래 길이 오른쪽 배치 */}
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <DeleteOutlineIcon style={{ marginRight: "10px" }} />{" "}
+                  {/* 쓰레기통 아이콘 추가 */}
+                  {Math.floor(song.length / 60000)}:
+                  {String((song.length % 60000) / 1000).padStart(2, "0")}{" "}
+                  {/* 음악 길이 형식 수정 */}
+                </div>
               </div>
             ))}
           </div>
