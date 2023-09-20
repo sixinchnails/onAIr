47f26269e3957f2f3a8f730675b7a6fe45ccb0d9
강현 <hyunleo@naver.com>
2023-09-21 00:13:35 +0900

    feat: axios 온케스트

--- client/src/component/PlayerPage/Music.tsx
+++ client/src/component/PlayerPage/Music.tsx
@@ -1,23 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import YouTube, { YouTubeProps } from "react-youtube";
 
 export const Music = () => {
   const dispatch = useDispatch();
   const radioDummyData = useSelector((state: RootState) => state.radioDummy);
   const navigate = useNavigate();
 
-  const handleAudioEnd = () => {
-    dispatch({ type: "INCREMENT_MUSIC_INDEX" }); // 인덱스 증가
-    navigate("/RadioPlayer");
-  };
+  // const handleAudioEnd = () => {
 
-  // 현재 인덱스에 맞게 음악 선택
-  const currentMusic = [
+  //   navigate("/RadioPlayer");
+  // };
+
+  const currentMusicKey = [
     `oncast_music_one`,
     `oncast_music_two`,
     `oncast_music_three`,
   ][radioDummyData.currentMusicIndex];
+
+  // 현재 선택된 음악의 youtubeId 가져오기
+  const currentYoutubeId = String(radioDummyData[currentMusicKey]);
 
   // 현재 노래의 제목, 아티스트, 앨범 표지 가져오기
   const currentMusicTitle =
@@ -26,6 +30,25 @@ export const Music = () => {
     radioDummyData.musicArtist[radioDummyData.currentMusicIndex];
   const currentMusicCover =
     radioDummyData.musicCover[radioDummyData.currentMusicIndex];
+
+  const onPlayerReady: YouTubeProps["onReady"] = event => {};
+
+  const handleVideoEnd = () => {
+    dispatch({ type: "INCREMENT_MUSIC_INDEX" }); // 인덱스 증가
+    navigate("/RadioPlayer"); // Radio.tsx로의 경로를 설정합니다.
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  useEffect(() => {
+    console.log("Current Music Index:", radioDummyData.currentMusicIndex);
+  }, [radioDummyData.currentMusicIndex]);
 
   return (
     <div
@@ -43,10 +66,12 @@ export const Music = () => {
         <h2>{currentMusicTitle}</h2> {/* 현재 노래 제목 렌더링 */}
         <p>{currentMusicArtist}</p> {/* 현재 아티스트 렌더링 */}
       </div>
-      <audio controls autoPlay onEnded={handleAudioEnd}>
+
+      {/* <audio controls autoPlay onEnded={handleAudioEnd}>
         <source src={radioDummyData[currentMusic] as string} type="audio/mp3" />
         Your browser does not support the audio element.
-      </audio>
+      </audio> */}
+
       {/* 오디오 플레이어 아래에 앨범 커버 이미지 렌더링 */}
       <div style={{ marginTop: "20px" }}>
         <img
@@ -55,6 +80,12 @@ export const Music = () => {
           style={{ width: "400px", height: "400px" }}
         />
       </div>
+      <YouTube
+        videoId={currentYoutubeId}
+        opts={opts}
+        onReady={onPlayerReady}
+        onEnd={handleVideoEnd} // 여기에 onEnd 이벤트 핸들러를 추가합니다.
+      />
     </div>
   );
 };
