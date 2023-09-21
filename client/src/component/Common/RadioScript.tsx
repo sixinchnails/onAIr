f68861c49cd7557bd72c167b7fe2267460fa368d
강현 <hyunleo@naver.com>
2023-09-22 01:55:16 +0900

    feat: 일단 redux 온캐스트 프로그래시브 바 완^^

--- client/src/component/Common/RadioScript.tsx
+++ client/src/component/Common/RadioScript.tsx
@@ -3,17 +3,21 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import styles from "./RadioScript.module.css";
 
-type RadioScriptsProps = {
-  djName: string;
-  script: string;
-};
+export const RadioScripts = () => {
+  // Redux에서 스크립트 데이터를 가져옵니다.
+  const radioDummyData = useSelector((state: RootState) => state.radioDummy);
+  const currentScript = [
+    `script_one`,
+    `script_two`,
+    `script_three`,
+    `script_four`,
+  ][radioDummyData.currentTTSIndex];
 
-export const RadioScripts = ({ djName, script }: RadioScriptsProps) => {
   return (
     <div className={styles.container}>
-      <h2>{djName}</h2> {/* djName을 props에서 가져옴 */}
+      <h2>{radioDummyData.djName}</h2>
       <hr className={styles.hrStyle} />
-      <p>{script}</p> {/* script를 props에서 가져옴 */}
+      <p>{radioDummyData[currentScript]}</p>
     </div>
   );
 };
--- client/src/component/PlayerPage/Music.tsx
+++ client/src/component/PlayerPage/Music.tsx
@@ -1,50 +1,219 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
 import { useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import YouTube, { YouTubeProps } from "react-youtube";
+import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 
-type MusicProps = {
-  musicFiles: any[];
+type YouTubePlayer = {
+  getCurrentTime: () => number;
+  getDuration: () => number;
+  seekTo: (seconds: number, allowSeekAhead?: boolean) => void;
+  playVideo: () => void;
+  pauseVideo: () => void;
 };
 
-export const Music = ({ musicFiles }: MusicProps) => {
-  const [currentMusicIndex, setCurrentMusicIndex] = useState(0); // 현재 재생 중인 음악의 인덱스
+export const Music = () => {
+  const dispatch = useDispatch();
+  const radioDummyData = useSelector((state: RootState) => state.radioDummy);
+  const navigate = useNavigate();
 
-  // YouTube 플레이어 설정
-  const opts: YouTubeProps["opts"] = {
-    height: "390",
-    width: "640",
+  const currentMusicKey = [
+    `oncast_music_one`,
+    `oncast_music_two`,
+    `oncast_music_three`,
+  ][radioDummyData.currentMusicIndex];
+
+  const currentYoutubeId = String(radioDummyData[currentMusicKey]);
+  const currentMusicTitle =
+    radioDummyData.musicTitle[radioDummyData.currentMusicIndex];
+  const currentMusicArtist =
+    radioDummyData.musicArtist[radioDummyData.currentMusicIndex];
+  const currentMusicCover =
+    radioDummyData.musicCover[radioDummyData.currentMusicIndex];
+
+  const [progress, setProgress] = useState(0); // Progress Bar의 진행 상태
+  const [player, setPlayer] = useState<YouTubePlayer | null>(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const opts = {
+    height: "0",
+    width: "0",
     playerVars: {
       autoplay: 1,
     },
   };
 
-  // 현재 재생 중인 음악의 정보
-  const currentMusic = musicFiles[currentMusicIndex];
+  const onPlayerReady = (event: { target: YouTubePlayer }) => {
+    setPlayer(event.target);
+  };
 
   const handleVideoEnd = () => {
-    const nextMusicIndex = currentMusicIndex + 1;
-    if (nextMusicIndex < musicFiles.length) {
-      setCurrentMusicIndex(nextMusicIndex); // 다음 음악으로 넘어갑니다.
-    } else {
-      setCurrentMusicIndex(0); // 모든 음악이 끝나면 처음으로 돌아갑니다.
+    dispatch({ type: "INCREMENT_MUSIC_INDEX" });
+    navigate("/RadioPlayer");
+  };
+
+  const updateProgress = () => {
+    if (player) {
+      const newCurrentTime = player.getCurrentTime();
+      const newDuration = player.getDuration();
+      setCurrentTime(newCurrentTime);
+      setDuration(newDuration);
+      setProgress((newCurrentTime / newDuration) * 100);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(updateProgress, 1000);
+    return () => clearInterval(interval);
+  }, [player]);
+
+  useEffect(() => {
+    console.log("Current Music Index:", radioDummyData.currentMusicIndex);
+  }, [radioDummyData.currentMusicIndex]);
+
+  const handleProgressBarClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (player && e.currentTarget) {
+      const rect = e.currentTarget.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const clickedValue = x / rect.width;
+      const newTime = clickedValue * player.getDuration();
+
+      player.seekTo(newTime); // 원하는 노래 지점으로 이동
+    }
+  };
+
+  const handlePlay = () => {
+    if (player) {
+      player.playVideo(); // 영상 재생
+    }
+  };
+
+  const handlePause = () => {
+    if (player) {
+      player.pauseVideo(); // 영상 일시정지
+    }
+  };
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  };
+
+  const handlePlayPause = () => {
+    if (player) {
+      if (isPlaying) {
+        player.pauseVideo();
+      } else {
+        player.playVideo();
+      }
+      setIsPlaying(!isPlaying);
     }
   };
 
   return (
     <div
-      style={{ backgroundColor: "#000104", height: "100vh", color: "white" }}
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${currentMusicCover})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundColor: "rgba(0, 1, 4, 0.9)",
+        height: "100%",
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      {/* 현재 재생 중인 음악의 제목과 아티스트를 출력합니다. */}
-      <h2>{currentMusic.title}</h2>
-      <p>{currentMusic.artist}</p>
+      {/* Album Info */}
+      <div style={{ textAlign: "center", zIndex: 2 }}>
+        <h2>{currentMusicTitle}</h2>
+        <p>{currentMusicArtist}</p>
+      </div>
 
-      <YouTube
-        videoId={currentMusic.youtubeId}
-        opts={opts}
-        onEnd={handleVideoEnd} // 음악이 끝나면 다음 음악으로 넘어갑니다.
+      {/* 진행 바, 현재/전체 시간, 재생/일시정지 아이콘 */}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          marginBottom: "20px",
+        }}
+      >
+        <div
+          style={{
+            width: "50%",
+            background: "#ccc",
+            cursor: "pointer",
+          }}
+          onClick={handleProgressBarClick}
+        >
+          <div
+            style={{
+              width: `${progress}%`,
+              background: "blue",
+              height: "4px",
+            }}
+          ></div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "50%",
+          }}
+        >
+          <span>{formatTime(currentTime)}</span>
+          <div>
+            {isPlaying ? (
+              <PauseIcon
+                onClick={handlePlayPause}
+                style={{ cursor: "pointer" }}
+              />
+            ) : (
+              <PlayArrowIcon
+                onClick={handlePlayPause}
+                style={{ cursor: "pointer" }}
+              />
+            )}
+          </div>
+          <span>{formatTime(duration)}</span>
+        </div>
+      </div>
+      {/* 현재 시간과 전체 영상 길이 */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "50%",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      ></div>
+
+      {/* Album Cover */}
+      <img
+        src={currentMusicCover}
+        alt="Album cover"
+        style={{ width: "400px", height: "400px" }}
       />
+
+      {/* YouTube Player */}
+      <YouTube
+        videoId={currentYoutubeId}
+        opts={opts}
+        onReady={onPlayerReady}
+        onEnd={handleVideoEnd}
+      />
+
+      {/* Play/Pause Button */}
+      <div style={{ marginTop: "20px" }}></div>
     </div>
   );
 };
