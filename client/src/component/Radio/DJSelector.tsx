59caa58cb7e4a0948c288bc6bff0b93db1a48eda
Yeon JeGyeong <yllh325@gmail.com>
2023-09-23 19:55:12 +0900

    Feat:온캐스트 생성 CSS 생성창이랑 DJ선택 모듈까지 CSS 초안은 잡음 전체적인 컬러 잡아야하는디 INPUT창 컬러가 안바뀌는데 어떻게해야하지

--- client/src/component/Radio/DJSelector.tsx
+++ client/src/component/Radio/DJSelector.tsx
@@ -1,71 +1,107 @@
 import React, { useState } from "react";
-import 여니 from "../../resources/여니.png";
-import 영우 from "../../resources/영우.png";
-import 흥애 from "../../resources/흥애.png";
-import { Button, Grid, Box } from "@mui/material";
+import dj1 from "../../resources/ncs1.gif";
+import dj2 from "../../resources/ncs2.gif";
+import dj3 from "../../resources/ncs3.gif";
+import dj4 from "../../resources/ncs4.gif";
+import dj5 from "../../resources/ncs5.gif";
+import { Grid, IconButton } from "@mui/material";
+import ArrowBackIos from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIos from "@mui/icons-material/ArrowForwardIos";
 
 type DJSelectorProps = {
   onSelect: (DJ: string) => void;
 };
 
 function DJSelector({ onSelect }: DJSelectorProps) {
-  const DJData = [
-    { name: "여니", imgSrc: 여니 },
-    { name: "영우", imgSrc: 영우 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-    { name: "흥애", imgSrc: 흥애 },
-  ];
+  const [selectedDJ, setSelectedDJ] = useState<string | null>(null);
 
-  const [visibleStartIndex, setVisibleStartIndex] = useState(0);
+  const preview1 = "/preview/아라.mp3";
+  const preview2 = "/preview/이안.mp3";
+  const preview3 = "/preview/고은.mp3";
+  const preview4 = "/preview/규원.mp3";
+  const preview5 = "/preview/기효.mp3";
+  const preview6 = "/preview/나오미.mp3";
+  const preview7 = "/preview/정영화.mp3";
+  const preview8 = "/preview/상도.mp3";
+  const preview9 = "/preview/안나.mp3";
+  const preview10 = "/preview/원탁.mp3";
+
+  const handleSelect = (DJName: string, soundSrc: string) => {
+    onSelect(DJName);
+    setSelectedDJ(DJName);
+    playSound(soundSrc);
+  };
+  const playSound = (soundSrc: string) => {
+    const audio = new Audio(soundSrc);
+    audio.play();
+  };
+
+  const DJData = [
+    { name: "아라", imgSrc: dj1, sound: preview1 },
+    { name: "이안", imgSrc: dj2, sound: preview2 },
+    { name: "고은", imgSrc: dj3, sound: preview3 },
+    { name: "규원", imgSrc: dj2, sound: preview4 },
+    { name: "기효", imgSrc: dj5, sound: preview5 },
+    { name: "나오미", imgSrc: dj3, sound: preview6 },
+    { name: "정영화", imgSrc: dj4, sound: preview7 },
+    { name: "상도", imgSrc: dj1, sound: preview8 },
+    { name: "안나", imgSrc: dj4, sound: preview9 },
+    { name: "원탁", imgSrc: dj5, sound: preview10 },
+  ];
+  const [centerIndex, setCenterIndex] = useState(3);
 
   const handleNext = () => {
-    setVisibleStartIndex((prev) => Math.min(prev + 5, DJData.length - 5));
+    setCenterIndex((prev) => Math.min(prev + 1, DJData.length - 4));
   };
 
   const handlePrev = () => {
-    setVisibleStartIndex((prev) => Math.max(prev - 5, 0));
+    setCenterIndex((prev) => Math.max(prev - 1, 3));
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <div>
-        <h2>DJ</h2>
-      </div>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <button onClick={handlePrev}>이전</button>
-        <div style={{ display: "flex", flexWrap: "wrap" }}>
-          {DJData.slice(visibleStartIndex, visibleStartIndex + 5).map(
-            (DJ, index) => (
-              <div key={index} style={{ margin: "5px" }}>
-                <button onClick={() => onSelect(DJ.name)}>
-                  <img
-                    src={DJ.imgSrc}
-                    alt={`DJ${DJ.name}`}
-                    style={{ height: "50px", width: "50px" }}
-                  />
-                </button>
-                <span>{`DJ ${DJ.name}`}</span>
-              </div>
-            )
-          )}
-        </div>
-        <button onClick={handleNext}>다음</button>
-      </div>
-    </div>
+    <Grid container alignItems="center" justifyContent="center" spacing={2}>
+      <Grid item>
+        <IconButton onClick={handlePrev} size="large">
+          <ArrowBackIos />
+        </IconButton>
+      </Grid>
+
+      {DJData.slice(centerIndex - 3, centerIndex + 4).map((DJ, index) => (
+        <Grid item key={index} style={{ textAlign: "center" }}>
+          <button
+            onClick={() => handleSelect(DJ.name, DJ.sound)}
+            style={{
+              height: "53px", // 보더의 두께만큼 증가시킵니다 (2px x 2)
+              width: "53px", // 보더의 두께만큼 증가시킵니다 (2px x 2)
+              margin: 0,
+              padding: 0,
+              border:
+                selectedDJ === DJ.name ? "2px solid red" : "2px solid black",
+              borderRadius: "5px", // 버튼을 둥글게 만듭니다
+              overflow: "hidden", // 이 부분은 이미지가 둥근 부분 밖으로 나가지 않도록 합니다
+            }}
+          >
+            <img
+              src={DJ.imgSrc}
+              alt={`${DJ.name}`}
+              style={{
+                height: "50px",
+                width: "50px",
+              }}
+            />
+          </button>
+          <div style={{ color: selectedDJ === DJ.name ? "red" : "black" }}>
+            {`${DJ.name}`}
+          </div>
+        </Grid>
+      ))}
+
+      <Grid item>
+        <IconButton onClick={handleNext} size="large">
+          <ArrowForwardIos />
+        </IconButton>
+      </Grid>
+    </Grid>
   );
 }
-
 export default DJSelector;
--- client/src/pages/HomePage/CreateRadio.tsx
+++ client/src/pages/HomePage/CreateRadio.tsx
@@ -8,186 +8,265 @@ import DJSelector from "../../component/Radio/DJSelector";
 import axios from "axios";
 
 import { requestWithTokenRefresh } from "../../utils/requestWithTokenRefresh ";
-import { Grid, TextField, Button, Typography } from '@mui/material';
+import { Grid, TextField, Button, Typography } from "@mui/material";
 
 const CreateRadio = () => {
-    const titleRef = useRef<HTMLInputElement>(null);
-    const [selectedTheme, setSelectedTheme] = useState("");
-    const contentRef = useRef<HTMLTextAreaElement>(null);
-    const [contentLength, setContentLength] = useState(0);
-    const [selectedDJ, setSelectedDJ] = useState("");
-    const navigate = useNavigate();
-    const [showButton, setShowButton] = useState(false);
+  const titleRef = useRef<HTMLInputElement>(null);
+  const [selectedTheme, setSelectedTheme] = useState("");
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+  const [contentLength, setContentLength] = useState(0);
+  const [selectedDJ, setSelectedDJ] = useState("");
+  const navigate = useNavigate();
+  const [showButton, setShowButton] = useState(false);
 
-    const handleCreate = () => {
-      const inputTitle = titleRef.current ? titleRef.current.value : "";
-      const inputContent = contentRef.current ? contentRef.current.value : "";
-      const inputTheme = selectedTheme;
-      const inputDJ = selectedDJ;
-      console.log("Title:", inputTitle);
-      console.log("Content:", inputContent);
-      console.log("inputTheme", inputTheme);
-      console.log("inputDJ", inputDJ);
-  
-      if (!inputTitle.trim()) {
-        alert("제목을 입력해주세요!");
-        return;
-      }
-      if (!inputContent.trim()) {
-        alert("내용을 입력해주세요");
-        return;
-        navigate;
-      }
-      if (!inputTheme.trim()) {
-        alert("테마를 선택해주세요");
-        return;
-      }
-      if (!inputDJ.trim()) {
-        alert("DJ를 선택해주세요");
-        return;
-      }
-      // navigate("/Loading");
-      setShowButton(true);
-    };
+  const handleCreate = () => {
+    const inputTitle = titleRef.current ? titleRef.current.value : "";
+    const inputContent = contentRef.current ? contentRef.current.value : "";
+    const inputTheme = selectedTheme;
+    const inputDJ = selectedDJ;
 
-    const handleThemeSelect = (theme: string) => {
-        setSelectedTheme(theme);
-    };
+    if (!inputTitle.trim()) {
+      alert("제목을 입력해주세요!");
+      return;
+    }
+    if (!inputContent.trim()) {
+      alert("내용을 입력해주세요");
+      return;
+      navigate;
+    }
+    if (!inputTheme.trim()) {
+      alert("테마를 선택해주세요");
+      return;
+    }
+    if (!inputDJ.trim()) {
+      alert("DJ를 선택해주세요");
+      return;
+    }
+    // navigate("/Loading");
+    setShowButton(true);
+  };
 
-    const handlDJSelect = (DJ: string) => {
-        setSelectedDJ(DJ);
-    };
+  const handleThemeSelect = (theme: string) => {
+    setSelectedTheme(theme);
+  };
 
-    const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setContentLength(e.target.value.length);
-    };
+  const handlDJSelect = (DJ: string) => {
+    setSelectedDJ(DJ);
+  };
 
-    const [fetchOncast, setFetchOncast] = useState(false);
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContentLength(e.target.value.length);
+  };
 
-    const handleOncastButtonClick = () => {
-        setFetchOncast(true);
-    };
+  const [fetchOncast, setFetchOncast] = useState(false);
 
-    const dispatch = useDispatch();
+  const handleOncastButtonClick = () => {
+    setFetchOncast(true);
+  };
 
-    type MusicItem = {
-      title: string;
-      artist: string;
-      duration: number;
-      albumCoverUrl: string;
-      youtubeId: string;
-    };
-  
-    useEffect(() => {
-      if (fetchOncast) {
-        requestWithTokenRefresh(() => {
-          return axios.get("http://localhost:8080/api/oncast/play/7", {
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem("accessToken"),
-            },
-            withCredentials: true,
-          });
-        }).then(response => {
-          console.log("Response Data:", response.data);
-          console.log("Music Data:", response.data.oncast.music);
-  
-          const data: {
-            djName: string;
-            ttsOne: string;
-            ttsTwo: string;
-            ttsThree: string;
-            ttsFour: string;
-            scriptOne: string;
-            scriptTwo: string;
-            scriptThree: string;
-            scriptFour: string;
-            music: {
-              title: string;
-              artist: string;
-              duration: number;
-              albumCoverUrl: string;
-              youtubeId: string;
-            }[];
-          } = response.data.oncast; // 여기서 .oncast를 추가했습니다.
-  
-          const music: MusicItem[] = response.data.oncast.music; // 여기서도 .oncast를 추가했습니다.
-  
-          // Redux 액션 디스패치: 데이터를 Redux 스토어에 저장합니다.
-  
-          // oncast 데이터 액션 디스패치
-          dispatch(
-            setRadioDummyData({
-              djName: data.djName,
-              tts_one: data.ttsOne,
-              tts_two: data.ttsTwo,
-              tts_three: data.ttsThree,
-              tts_four: data.ttsFour,
-              script_one: data.scriptOne,
-              script_two: data.scriptTwo,
-              script_three: data.scriptThree,
-              script_four: data.scriptFour,
-              oncast_music_one: data.music[0].youtubeId,
-              oncast_music_two: data.music[1].youtubeId,
-              oncast_music_three: data.music[2].youtubeId,
-            })
-          );
-  
-          // 음악 데이터 액션 디스패치
-          dispatch(
-            setMusicInfo({
-              musicTitle: music.map(m => m.title),
-              musicArtist: music.map(m => m.artist),
-              musicLength: music.map(m => m.duration),
-              musicCover: music.map(m => m.albumCoverUrl),
-            })
-          );
-          setFetchOncast(false); // 통신 후 상태 값을 초기화합니다.
-          navigate("/RadioPlayer");
+  const dispatch = useDispatch();
+
+  type MusicItem = {
+    title: string;
+    artist: string;
+    duration: number;
+    albumCoverUrl: string;
+    youtubeId: string;
+  };
+
+  useEffect(() => {
+    if (fetchOncast) {
+      requestWithTokenRefresh(() => {
+        return axios.get("http://localhost:8080/api/oncast/play/7", {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("accessToken"),
+          },
+          withCredentials: true,
         });
-      }
-    }, [fetchOncast, dispatch]);
-  
-    /**AXIOS */
-    //여기서 POST매핑하면 끝.
+      }).then((response) => {
+        console.log("Response Data:", response.data);
+        console.log("Music Data:", response.data.oncast.music);
 
-    return (
-        <div className={styles.container}>
-            <NavBar className={styles.navbarSticky} />
-            <div>
-            <Grid container spacing={3} className={styles.radioWrapper}>
-                <Grid item xs={12} style={{ textAlign: "center" }}>
-                    <Typography variant="h4">TITLE</Typography>
-                    <TextField fullWidth inputRef={titleRef} variant="outlined" />
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography variant="h4">THEME</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    {["JOYFUL", "SENSITIVE", "HOPEFUL", "CHILL", "AGGRESSIVE", "ROMANTIC", "RETRO", "DRAMATIC", "FUNKY"].map(theme => (
-                        <Button key={theme} onClick={() => handleThemeSelect(theme)}>{theme}</Button>
-                    ))}
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="h4">
-                        STORY
-                        <span>{`${contentLength}/1000`}</span>
-                    </Typography>
-                    <TextField fullWidth multiline rows={4} inputRef={contentRef} onChange={handleContentChange} variant="outlined" />
-                </Grid>
-                <Grid item xs={12}>
-                    <DJSelector onSelect={handlDJSelect} />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button onClick={handleCreate} style={{ marginRight: "10px" }}>생성</Button>
-                    <Link to="/"><Button>취소</Button></Link>
-                </Grid>
+        const data: {
+          djName: string;
+          ttsOne: string;
+          ttsTwo: string;
+          ttsThree: string;
+          ttsFour: string;
+          scriptOne: string;
+          scriptTwo: string;
+          scriptThree: string;
+          scriptFour: string;
+          music: {
+            title: string;
+            artist: string;
+            duration: number;
+            albumCoverUrl: string;
+            youtubeId: string;
+          }[];
+        } = response.data.oncast; // 여기서 .oncast를 추가했습니다.
+
+        const music: MusicItem[] = response.data.oncast.music; // 여기서도 .oncast를 추가했습니다.
+
+        // Redux 액션 디스패치: 데이터를 Redux 스토어에 저장합니다.
+
+        // oncast 데이터 액션 디스패치
+        dispatch(
+          setRadioDummyData({
+            djName: data.djName,
+            tts_one: data.ttsOne,
+            tts_two: data.ttsTwo,
+            tts_three: data.ttsThree,
+            tts_four: data.ttsFour,
+            script_one: data.scriptOne,
+            script_two: data.scriptTwo,
+            script_three: data.scriptThree,
+            script_four: data.scriptFour,
+            oncast_music_one: data.music[0].youtubeId,
+            oncast_music_two: data.music[1].youtubeId,
+            oncast_music_three: data.music[2].youtubeId,
+          })
+        );
+
+        // 음악 데이터 액션 디스패치
+        dispatch(
+          setMusicInfo({
+            musicTitle: music.map((m) => m.title),
+            musicArtist: music.map((m) => m.artist),
+            musicLength: music.map((m) => m.duration),
+            musicCover: music.map((m) => m.albumCoverUrl),
+          })
+        );
+        setFetchOncast(false); // 통신 후 상태 값을 초기화합니다.
+        navigate("/RadioPlayer");
+      });
+    }
+  }, [fetchOncast, dispatch]);
+
+  /**AXIOS */
+  //여기서 POST매핑하면 끝.
+
+  return (
+    <div>
+      <NavBar />
+      <div className={styles.container}>
+        <div>
+          <Grid container spacing={3} className={styles.oncastCreate}>
+            <Grid item container xs={12} alignItems="center" spacing={2}>
+              <Grid item xs={2}>
+                <Typography variant="h5" className={styles.itemTitle}>
+                  TITLE
+                </Typography>
+              </Grid>
+              <Grid item xs={9.5}>
+                <TextField
+                  style={{ color: "red" }}
+                  className={styles.titleInput}
+                  fullWidth
+                  inputRef={titleRef}
+                  variant="outlined"
+                />
+              </Grid>
             </Grid>
-            </div>
-            {showButton && (
-                <Button onClick={handleOncastButtonClick} variant="contained" color="primary">온캐스트 들으러 가기</Button>
-            )}
+
+            <Grid item container xs={12} alignItems="center" spacing={2}>
+              <Grid item xs={2}>
+                <Typography variant="h5" className={styles.itemTitle}>
+                  THEME
+                </Typography>
+              </Grid>
+              <Grid item xs={10} className={styles.themeSelect}>
+                {[
+                  "JOYFUL",
+                  "SENSITIVE",
+                  "HOPEFUL",
+                  "CHILL",
+                  "AGGRESSIVE",
+                  "ROMANTIC",
+                  "RETRO",
+                  "DRAMATIC",
+                  "FUNKY",
+                  "EXOTIC",
+                  "ACOUSTIC",
+                  "NOSTALGIC",
+                  "DREAMY",
+                  "UPBEAT",
+                  "ENERGETIC",
+                  "MELANCHOLY",
+                  "SAD",
+                ].map((theme) => (
+                  <Button
+                    key={theme}
+                    onClick={() => handleThemeSelect(theme)}
+                    className={
+                      theme === selectedTheme ? styles.selectedTheme : ""
+                    }
+                  >
+                    {theme}
+                  </Button>
+                ))}
+              </Grid>
+            </Grid>
+
+            <Grid item container xs={12} alignItems="flex-start" spacing={2}>
+              <Grid item xs={2}>
+                <Typography className={styles.itemTitle} variant="h5">
+                  STORY
+                </Typography>
+              </Grid>
+              <Grid item xs={9.5} style={{ textAlign: "right" }}>
+                <TextField
+                  fullWidth
+                  multiline
+                  rows={4}
+                  inputRef={contentRef}
+                  onChange={handleContentChange}
+                  variant="outlined"
+                  className={styles.storyInput}
+                />
+                <div
+                  className={styles.typingLimit}
+                >{`${contentLength}/1000`}</div>
+              </Grid>
+            </Grid>
+
+            <Grid item container xs={12} alignItems="center" spacing={2}>
+              <Grid item xs={2}>
+                <Typography variant="h5" className={styles.itemTitle}>
+                  DJ
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <DJSelector onSelect={handlDJSelect} />
+              </Grid>
+            </Grid>
+
+            <Grid item container xs={12} justifyContent="flex-end">
+              <Button
+                className={styles.createButton}
+                onClick={handleCreate}
+                style={{ marginRight: "10px" }}
+              >
+                생성
+              </Button>
+              <Link to="/">
+                <Button className={styles.cancleButton}>취소</Button>
+              </Link>
+            </Grid>
+          </Grid>
         </div>
-    );
+        {showButton && (
+          <Button
+            onClick={handleOncastButtonClick}
+            variant="contained"
+            color="primary"
+          >
+            온캐스트 들으러 가기
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CreateRadio;
