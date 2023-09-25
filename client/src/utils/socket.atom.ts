66c9da9c982e1d4832542cca5d4fd4cb97c0e924
강현 <hyunleo@naver.com>
2023-09-25 15:33:07 +0900

    feat: 시간에 맞는 영상 불러오기

--- client/src/utils/socket.atom.ts
+++ client/src/utils/socket.atom.ts
@@ -4,8 +4,25 @@ console.log("초기화 됨");
 
 let socketManager = SocketManager.getInstance();
 
-// 웹소켓 연결
-export const socketConnection = async () => {
+export type MusicData = {
+  type: string;
+  operation: string;
+  data: {
+    type: string;
+    typePlayedTime: number;
+    path: string;
+    startTime: number;
+    playedTime: number;
+    length: number;
+    title: string;
+    artist: string;
+    image: string;
+  };
+};
+
+export const socketConnection = async (
+  onDataReceived: (data: MusicData) => void
+) => {
   let stompClient = socketManager.connect();
   console.log("socketConnection 들어오!");
 
@@ -18,11 +35,19 @@ export const socketConnection = async () => {
     stompClient.subscribe("/topic", message => {
       console.log("전체 요청 들어옴!!!");
       console.log(message.body);
+
+      // 이 부분에 음악 데이터를 저장하는 로직을 추가
+      let receivedData = JSON.parse(message.body);
+      onDataReceived(receivedData); // 여기서 callback 함수를 호출하여 데이터를 전달합니다.
     });
 
     stompClient.subscribe("/user/queue", message => {
       console.log("나 한테만 요청 들어옴!!!");
       console.log(message.body);
+
+      // 이 부분에 음악 데이터를 저장하는 로직을 추가
+      let receivedData = JSON.parse(message.body);
+      onDataReceived(receivedData); // 여기서 callback 함수를 호출하여 데이터를 전달합니다.
     });
 
     stompClient.publish({ destination: "/app/subscribe", body: "" });
