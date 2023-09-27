306798d56847cc6ade73b79cdc661e71695727a3
Yeon JeGyeong <yllh325@gmail.com>
2023-09-27 15:56:42 +0900

    Fix:Config Bean 빈 이름 재설정

--- zizon/src/main/java/com/b302/zizon/config/RestTemplateConfig.java
+++ zizon/src/main/java/com/b302/zizon/config/RestTemplateConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-    @Bean
+    @Bean(name = "FlaskRestTemplate")
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
