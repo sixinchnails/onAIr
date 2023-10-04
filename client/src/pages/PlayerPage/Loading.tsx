36fb68b253e91d86a309bbbc9b8c8cd0b4260cbb
박기현 <qkrrlgus114@naver.com>
2023-10-04 13:14:20 +0900

    Fix : 로딩창 수정

--- client/src/pages/PlayerPage/Loading.tsx
+++ client/src/pages/PlayerPage/Loading.tsx
@@ -1,15 +1,35 @@
-import { Link } from "react-router-dom";
-import styles from "./Loading.module.css";
+import React, { useState, useEffect } from 'react';
+import styles from './Loading.module.css';
 
 export const Loading = () => {
+  const [ballPosition, setBallPosition] = useState({ x: 0, y: 0 });
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    placeBallRandomly();
+  }, []);
+
+  const placeBallRandomly = () => {
+    const x = Math.random() * (window.innerWidth - 50); // 50 is approximate ball size
+    const y = Math.random() * (window.innerHeight - 50);
+    setBallPosition({ x, y });
+  };
+
   return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.loader}>
-          <img src={"/gif/loading4.gif"} alt="Loading..." />
-        </div>
-        <h2 style={{ color: "white" }}>로딩중...</h2>
+    <div className={styles.container}>
+      <div className={styles.loader}>
+        <img src={"/gif/loading4.gif"} alt="Loading..." />
       </div>
+      <h2 style={{ color: "white" }}>로딩중...</h2>
+      <div className={styles.score}>Score: {score}</div>
+      <div 
+        className={styles.ball} 
+        style={{ left: ballPosition.x, top: ballPosition.y }} 
+        onClick={() => {
+          setScore(score + 1);
+          placeBallRandomly();
+        }}
+      />
     </div>
   );
 };
