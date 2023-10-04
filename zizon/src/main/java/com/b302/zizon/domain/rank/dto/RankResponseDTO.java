949dc4272576e94122dc875be6fabd7a290d976f
박기현 <qkrrlgus114@naver.com>
2023-10-04 23:45:19 +0900

    Fix : 프롬프트 수정

---
+++ zizon/src/main/java/com/b302/zizon/domain/rank/dto/RankResponseDTO.java
@@ -0,0 +1,10 @@
+package com.b302.zizon.domain.rank.dto;
+
+import lombok.Data;
+
+@Data
+public class RankResponseDTO {
+
+    private Long userId;
+    private Long record;
+}
--- zizon/src/main/java/com/b302/zizon/domain/rank/repository/RankRepository.java
+++ zizon/src/main/java/com/b302/zizon/domain/rank/repository/RankRepository.java
@@ -1,13 +1,19 @@
 package com.b302.zizon.domain.rank.repository;
 
 import com.b302.zizon.domain.rank.entity.UserRank;
+import com.b302.zizon.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RankRepository extends JpaRepository<UserRank, Long> {
 
     Optional<UserRank> findByUserUserId(Long userId);
+
+    @Query(value = "SELECT r FROM UserRank r ORDER BY r.record ASC")
+    List<UserRank> findTop10ByRecord();
 }
