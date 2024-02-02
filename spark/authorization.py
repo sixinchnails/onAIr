e37fc61366c252229b225b526d6ce323cec59ecb
최재용 <johnchoi213.jc@gmail.com>
2024-02-03 05:09:32 +0900

    Update authorization.py

--- spark/authorization.py
+++ spark/authorization.py
@@ -1,8 +1,12 @@
 import tekore as tk
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 def authorize():
-    CLIENT_ID = "2721a5cc2cca43b5bf41cb824a4ac323"
-    CLIENT_SECRET = "999e01f4dd974aca86db9a4adca85991"
+    CLIENT_ID = config['SPOTIFY']['CLIENT_ID']
+    CLIENT_SECRET = config['SPOTIFY']['CLIENT_SECRET']
     app_token = tk.request_client_token(CLIENT_ID, CLIENT_SECRET)
 
     return tk.Spotify(app_token)
