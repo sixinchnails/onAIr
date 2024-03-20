076cbf848b8e4481d6adafc984f628004a1f8ce7
최재용 <johnchoi213.jc@gmail.com>
2024-03-20 15:06:36 +0900

    FIX: Must replace cursors with sessions.

--- data/orm/mysql/entities/Audio_Features.py
+++ data/orm/mysql/entities/Audio_Features.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, Float, String
-from sqlalchemy.orm import declarative_base
 from data.orm.mysql.DbUtils import *
 
 Base = declarative_base()
@@ -12,12 +11,12 @@ cursor = conn.cursor()
 class Audio_Features(Base):
     __tablename__ = 'audio_features'
 
-    feature_id = Column(String(500), primary_key=True, autoincrement=True)
-    mode = Column(Integer, nullable=False)
-    key = Column(Integer, nullable=False)
-    duration = Column(Integer, nullable=False)
-    tempo = Column(Float, nullable=False)
-    time_signature = Column(Integer, nullable=False)
+    track_feature_id = Column(String(500), primary_key=True, autoincrement=True)
+    mode = Column(Integer)
+    key = Column(Integer)
+    duration = Column(Integer)
+    tempo = Column(Float)
+    time_signature = Column(Integer)
 
     # Features used in Regression
     acousticness = Column(Float, nullable=False)
@@ -29,3 +28,32 @@ class Audio_Features(Base):
     loudness = Column(Float, nullable=False)
     speechiness = Column(Float, nullable=False)
     valence = Column(Float, nullable=False)
+
+    @classmethod
+    def create_instance(cls, row):
+        instance = cls(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+
+        instance.track_feature_id = row[0]
+
+        return instance
+
+    @classmethod
+    def getExclusiveFeatures(cls):
+        sql = """
+            SELECT id, acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, valence FROM audio_features
+        """
+
+        result_rows = cursor.execute(sql).fetchall()
+
+        return [cls.create_instance(row) for row in result_rows]
+
+    @classmethod
+    def getAudioFeatures(cls):
+        sql = """
+            SELECT * FROM audio_features
+        """
+
+
+        result_rows = cursor.execute(sql).fetchall()
+
+        return [cls.create_instance(row) for row in result_rows]
--- data/orm/mysql/entities/Tracks.py
+++ data/orm/mysql/entities/Tracks.py
@@ -12,16 +12,47 @@ cursor = conn.cursor()
 class Tracks(Base):
     __tablename__ = 'tracks'
 
-    feature_id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(500), nullable=False)
-    artist = Column(String(500), nullable=False)
-    duration = Column(Integer, nullable=False)
-    disc_number = Column(Integer, nullable=False)
-    explicit = Column(Integer, nullable=False)
-    audio_feature_id = Column(String(500), nullable=False)
+    track_id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(500))
+    artist = Column(String(500))
+    duration = Column(Integer)
+    disc_number = Column(Integer)
+    explicit = Column(Integer)
     preview_url = Column(String(500))
-    track_number = Column(Integer, nullable=False)
+    track_number = Column(Integer)
     popularity = Column(Float, nullable=False)
-    is_playable = Column(Integer, nullable=False)
-    album_cover_url = Column(String(500), nullable=False)
-    music_url = Column(String(500), nullable=False)
+    is_playable = Column(Integer)
+    album_cover_url = Column(String(500))
+    music_url = Column(String(500))
+
+    @classmethod
+    def create_instance(cls, row):
+        instance = cls(row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11])
+
+        instance.id = row[0]
+
+        return instance
+
+    @classmethod
+    def create_popularity_instance(cls, row):
+        instance = cls(row[1])
+
+        instance.id = row[0]
+
+        return instance
+
+    @classmethod
+    def getTracks(cls):
+        sql = "SELECT * FROM tracks"
+
+        result_rows = cursor.execute(sql).fetchall()
+
+        return [cls.create_instance(row) for row in result_rows]
+
+    @classmethod
+    def getPopularity(cls):
+        sql = "SELECT id, popularity from tracks"
+
+        result_rows = cursor.execute(sql).fetchall()
+
+        return [cls.create_popularity_instance(row) for row in result_rows]
